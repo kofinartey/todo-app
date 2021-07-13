7d089bb44b7ab6698592ecac34206ed8ef4ee265
Kofi Nartey <eugeneahulu15@live.com>
2021-07-13 02:23:39 +0000

    added local storage

--- src/contexts/TodosContext.js
+++ src/contexts/TodosContext.js
@@ -24,69 +24,16 @@ const todoReducer = (state, action) => {
   }
 };
 
-const initialTodos = [
-  { id: 1, task: "Go shopping", completed: false, active: false },
-  { id: 2, task: "Study a new topic", completed: false, active: false },
-];
-
 export function TodoProvider(props) {
+  //fetch todos from local storage if there be any
+  const initialTodos = JSON.parse(localStorage.getItem("todos") || "[]");
   const [todos, dispatch] = useReducer(todoReducer, initialTodos);
 
-  // //fetch todos from local storage
-  // useEffect(() => {
-  //   const data = localStorage.getItem("todos");
-  //   if (data) {
-  //     setTodos(JSON.parse(data));
-  //   }
-  // }, []);
-  // //save todos in local storage
-  // useEffect(() => {
-  //   localStorage.setItem("todos", JSON.stringify(todos));
-  // }, [todos]);
+  //save todos in local storage
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
 
-  // const addTodo = (newTodo) => {
-  //   setTodos((todos) => [
-  //     ...todos,
-  //     { id: uuidv4(), task: newTodo, completed: false, active: false },
-  //   ]);
-  // };
-
-  // const toggleCompletion = (id) => {
-  //   const newTodos = todos.map((todo) =>
-  //     todo.id === id ? { ...todo, completed: !todo.completed } : todo
-  //   );
-  //   setTodos(newTodos);
-  // };
-
-  // const toggleCompletion = (id) => {
-  //   const newTodos = todos.map((todo) => {
-  //     if (todo.id === id) {
-  //       return { ...todo, completed: !todo.completed };
-  //     }
-  //     return todo;
-  //   });
-  //   setTodos(newTodos);
-  // };
-
-  // const removeTodo = (id) => {
-  //   const updatedTodos = todos.filter((todo) => todo.id !== id);
-  //   setTodos(updatedTodos);
-  // };
-
-  // const clearCompleted = () => {
-  //   console.log("clear attempted");
-  //   const updatedTodos = todos.filter((todo) => !todo.completed);
-  //   setTodos(updatedTodos);
-  // };
-
-  // const value = {
-  //   todos,
-  //   setTodos,
-  //   addTodo,
-  //   toggleCompletion,
-  //   removeTodo,
-  //   clearCompleted,
-  // };
   return (
     <TodoContext.Provider value={{ todos, dispatch }}>
       {props.children}
