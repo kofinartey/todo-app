720ab38cd9bee18e64f20c819f8e03582d431914
Kofi Nartey <eugeneahulu15@live.com>
2021-07-13 02:29:27 +0000

    removed console warning

--- src/contexts/TodosContext.js
+++ src/contexts/TodosContext.js
@@ -1,4 +1,4 @@
-import React, { useReducer, useState, useEffect, createContext } from "react";
+import React, { useReducer, useEffect, createContext } from "react";
 import { v4 as uuidv4 } from "uuid";
 
 export const TodoContext = createContext();
